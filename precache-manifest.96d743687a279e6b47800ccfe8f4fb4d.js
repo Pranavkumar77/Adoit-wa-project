c2fdfca07ca58f442348558cd733f76ded360920
Pranavkumar77 <tanmaypatil686@gmail.com>
2021-02-26 13:22:05 +0530

    Updates

---
+++ precache-manifest.96d743687a279e6b47800ccfe8f4fb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b27f800495c5743d0ecb002764bc1e3",
+    "url": "/Adoit-wa-project/index.html"
+  },
+  {
+    "revision": "5116475c011e80495595",
+    "url": "/Adoit-wa-project/static/css/main.4bde9f14.chunk.css"
+  },
+  {
+    "revision": "80dde7114264f73fde77",
+    "url": "/Adoit-wa-project/static/js/2.a2b28672.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Adoit-wa-project/static/js/2.a2b28672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5116475c011e80495595",
+    "url": "/Adoit-wa-project/static/js/main.cc3404ac.chunk.js"
+  },
+  {
+    "revision": "da530ce990eafc1fe01d",
+    "url": "/Adoit-wa-project/static/js/runtime-main.15247c40.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Adoit-wa-project/static/media/bars-solid.b3f49afd.svg"
+  }
+]);
